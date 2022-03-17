76b489510304aaef0df0803b4aa2a8857b4bb0e0
murdoch2439 <murdochkhallz@gmail.com>
2022-03-17 17:55:08 +0100

    Setting home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,10 +2,12 @@ import React, {FunctionComponent} from 'react';
 import "./home.css"
 
 
-
 const Home : FunctionComponent =()=>{
     return(
-        <div className={"home__container"}>Home</div>
+        <div className={"home__container"}>
+            <div className={"left"}>Left</div>
+            <div className={"right"}>right</div>
+        </div>
     )
 
 }
